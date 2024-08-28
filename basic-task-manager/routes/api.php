37760c4cfdd85ffe540d1d1fd698596608b2938b
Sujit Kumar Roy <sujitkumarroyy@gmail.com>
2024-08-29 00:31:47 +0600

    Modify api method

--- basic-task-manager/routes/api.php
+++ basic-task-manager/routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::get('/tasks/{id}', [TaskController::class, 'getTask']);
     Route::post('/tasks/create', [TaskController::class, 'store']);
-    Route::post('/tasks/{id}/update', [TaskController::class, 'update']);
+    Route::patch('/tasks/{id}/update', [TaskController::class, 'update']);
     Route::delete('/tasks/{id}/delete', [TaskController::class, 'destroy']);
     Route::post('/tasks/share', [TaskController::class, 'share']);
 
